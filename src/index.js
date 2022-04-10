4566471bc4448eac3909972f0fe83b6ad6c6ffa8
Elena-Kovalevskaya <elena.kovalevskaya6@gmail.com>
2022-04-10 14:29:40 +0300

    init: start weather-app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p>Hello,my Friend!</p>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
